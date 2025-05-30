25bb065a4445c02777b320070cee4a0b77133e98
DanieleCorsini <daniele.corsini@action-ict.it>
2025-05-30 12:37:37 +0200

    Modifica path CUSTOMER-SERVICE: impostato path di "base" a "/api/customer/"

--- customer/src/main/java/com/actionict/customer/controller/CityController.java
+++ customer/src/main/java/com/actionict/customer/controller/CityController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/customers/cities")
+@RequestMapping("/cities")
 public class CityController extends EntityController<City> {
 
     public CityController(CityService service) {
